32913625d008da423b514f0a6c154aa3f2f3d710
Sacxer <sacxer24@gmail.com>
2025-10-14 17:39:20 -0500

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/entregable3/',
+  base: '/ApiSimpsons_2025IIg1_SanjuanValentina_TorresSantiago/',
   plugins: [react()],
 })
